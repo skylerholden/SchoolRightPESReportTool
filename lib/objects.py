a73a744e02c3004822b9817456734bd64b98ec2b
Ryan den Otter <ryan.denotter@kingsmenservices.org>
2020-07-11 13:27:48 -0600

    Committing the initial definition of the objects

---
+++ lib/objects.py
@@ -0,0 +1,17 @@
+
+
+class Response:
+
+    def __init__(self, response, question_id ):
+        self.response = response
+        self.question_id = question_id
+
+class Question:
+
+    def __init__(self, question):
+        self.question = question
+
+class Responder:
+
+    def __init__(self, responder_group_id):
+        pass
