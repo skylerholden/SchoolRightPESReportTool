02bc96ca25292fa1b8c9cfb327c8e76865835fba
Ryan den Otter <ryan.denotter@kingsmenservices.org>
2020-07-11 17:21:36 -0600

    Add timer

--- school_right_pes_report_tool.py
+++ school_right_pes_report_tool.py
@@ -1,14 +1,20 @@
 from lib.parser import Parser
+from lib.timer import Timer
 
+timer = Timer()
+timer.start()
 file_path = "data/PES-Hanoi.csv"
 
 parser = Parser()
 
 parser.parse(file_path)
 
+"""
 for responder_group in parser.response_groups:
     print(responder_group.name)
     for response in responder_group.responses:
         print(f'{response.question.value}: {response.answer}')
 
 print(len(parser.response_groups[0].responses))
+"""
+timer.stop()
